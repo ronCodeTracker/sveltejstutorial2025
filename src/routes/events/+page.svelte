690c52c91d3c1afedef2d9bd70638dcd88001a8d
Ronald <2fixelectronics777@gmail.com>
2025-04-09 01:53:47 -0500

    add more mod

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -83,7 +83,9 @@ import sound from './sound.mp3';
  
 </div>
 
-
+<div class="card2">
+	<a href="./extracomponents" class="homeLink">Extra</a>
+</div>
 
 <!-- title    -->
 <h1 class="titleEvents">Events</h1>
@@ -172,9 +174,25 @@ decrement={() => value -= 1}
             max-width: 300px;
 			max-height: 100px;
             text-align: center;
+			display: inline;
 			
         }
        
+		.card2 {
+            background-color: #36454F; /* Light cyan color */
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            max-width: 300px;
+			max-height: 100px;
+            text-align: center;
+			display: inline;
+			
+			
+        }
+
+
+
 	a {
 	
 		text-decoration: none;
